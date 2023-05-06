e1294e8744622c1eda2dc929db8195a280a03676
peytonhobson <peyton.hobson1@gmail.com>
2023-05-05 22:55:17 -0600

    new csp attempt

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => [
       contentSecurityPolicy: {
         directives: {
           'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
-          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('AWS_BUCKET_NAME')}.s3.${env('AWS_REGION')}.amazonaws.com`],
-          'media-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('AWS_BUCKET_NAME')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', '*.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', '*.amazonaws.com'],
         },
       }
     },
